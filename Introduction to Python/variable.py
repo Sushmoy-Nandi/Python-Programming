6e504ae6c98b149bc662f6ab12b671ae70606c7f
“Sushmoy <learnsushmoysk1091@gmail.com>
2025-03-20 11:22:49 +0600

    Introduction to Python added

---
+++ Introduction to Python/variable.py
@@ -0,0 +1,26 @@
+age=45
+interest_rate=2.5
+name="Sushmoy Nandi"
+district='Jashore'
+is_single=True
+is_sleeping=False
+
+print(age+interest_rate)
+print(district)
+
+print(type(age))
+print(type(interest_rate))
+# print(type(district))
+print(type(is_single))
+"""
+    This is 
+    multiline comment.
+"""
+
+# Ctrl + / : Single line of comment
+# Alt + Shift + A : Doc string or multi line comment
+
+
+print('Sushmoy Nandi'+ ' ' +'is a student')
+text=f"Sushmoy Nandi age {age} living in {district}"
+print(text)
