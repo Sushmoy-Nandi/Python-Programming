43c128411901c38920e56aca9bcc9a0942546d42
“Sushmoy <learnsushmoysk1091@gmail.com>
2025-04-13 18:42:53 +0600

    while loop, break and continue operation added

---
+++ Introduction to Python/break_continue.py
@@ -0,0 +1,23 @@
+# break
+# num = 1
+# while num <= 10:
+#     print(num)
+#     num = num+1
+#     if num == 5:
+#         break
+
+# Continue
+# num = 1
+# while num <= 10:
+#     num = num+1
+#     if num == 5:
+#         continue
+#     print(num)
+
+num = 0
+while num <= 10:
+    num = num+1
+    if num%2==1:
+        continue
+    print(num)
+
