595eb7f86197e19841ee18ce41ba6821ae2d395d
“Sushmoy <learnsushmoysk1091@gmail.com>
2025-04-14 19:10:02 +0600

    HomeWork added

---
+++ Introduction to Python/HOMEWORK.py
@@ -0,0 +1,13 @@
+# num1 = input('Enter first number : ')
+# num2 = input('Enter first number : ')
+# num3 = input('Enter first number : ')
+
+# if (num1 > num2 and num1 > num3):
+#     print(num1, ' is the largest number')
+# elif (num2 > num1 and num2 > num3):
+#     print(num2, ' is the largest number')
+# else:
+#     print(num3, ' is the largest number')
+
+for i in range(39,69):
+    print(i)
