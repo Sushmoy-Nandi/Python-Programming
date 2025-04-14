8f7c695d1af6ee9c7e1c53677b0f06c30b02db64
“Sushmoy <learnsushmoysk1091@gmail.com>
2025-04-14 18:59:22 +0600

    for loop added

---
+++ Introduction to Python/for_loop.py
@@ -0,0 +1,24 @@
+numbers = [5, 10, 15, 20, 25]
+sum=0
+for num in numbers:
+    print(num)
+    sum=sum+num
+print(sum)
+
+
+text ='Sushmoy Nandi'
+for char in text:
+    print(char)
+
+# numbers1=[1,2,3,4,5,6,7,8,9,10]
+range(1,10)
+
+print(range(1,10))
+
+
+for i in range(1,10,2):
+    print(i)
+
+friends=['rabbi','asif','rahim']
+for friend in friends:
+    print(friend)
